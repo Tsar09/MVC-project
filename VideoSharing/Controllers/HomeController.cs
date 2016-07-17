094e3cf338f9d8a667b23b9ac14bb9cbe6371b50
Tsar09 <tsar0911@gmail.com>
2016-07-17 11:51:24 +0300

    HomeController

---
+++ VideoSharing/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using VideoSharing.Business;
+using VideoSharing.DAL;
+using VideoSharing.Filters;
+
+namespace VideoSharing.Controllers
+{
+    [ExceptionFilterAttribute]
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string searchString)
+        {
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                if(!User.IsInRole("admin"))
+                    return RedirectToAction("People", "User", new { searchString = searchString});
+                else
+                {
+                    return RedirectToAction("PeopleTable", "User", new { searchString = searchString, sortOrder = "", filterValue = "" });
+                }
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+            return View();
+        }
+
+        public ActionResult PrivacyPolicy()
+        {
+          
+            return View();
+        }
+
+        public ActionResult Change(string languageAbbreviation)
+        {
+            if (languageAbbreviation != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(languageAbbreviation);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(languageAbbreviation);
+            }
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = languageAbbreviation;
+            Response.Cookies.Add(cookie);
+            return View("Index");
+        }
+    }
+}
---
+++ VideoSharing/Helper/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoSharing
+{
+    public static class Helper
+    {
+        public static MvcHtmlString If(this MvcHtmlString value, bool evaluation)
+        {
+            return evaluation ? value : MvcHtmlString.Empty;
+        }
+    }
+}
